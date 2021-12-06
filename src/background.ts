9a1f1c9e515e59fddda623f55f56bcb092a42c0c
Tomohiro Murota <tomohiro.murota@gmail.com>
2021-12-07 01:15:28 +0900

    Fix not to call addListener in another listener

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,9 @@
 import { parseDateString, formatWithTimezone } from './dateUtils'
 
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: Should be user preferences.
-  const targetTimezones = ["Asia/Tokyo", "Europe/London", "America/New_York"]
+// TODO: Should be user preferences.
+const targetTimezones = ["Asia/Tokyo", "Europe/London", "America/New_York"]
 
+chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.removeAll()
   chrome.contextMenus.create({
     "id": "TimezoneTravelerRoot",
@@ -28,67 +28,67 @@ chrome.runtime.onInstalled.addListener(() => {
     "type": "separator",
     "contexts": ["all"],
   })
-
-  chrome.contextMenus.onClicked.addListener(
-    async (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
-      const storageResults = await chrome.storage.local.get(["convertedSelections"])
-      const convertedSelections: string[] = storageResults.convertedSelections || []
-
-      const menuItemId = info.menuItemId.toString()
-      if (menuItemId === "TimezoneTravelerConvert") {
-        try {
-          const currentSelection = info.selectionText
-          if (!currentSelection) {
-            throw new Error("Text not seelcted.")
-          }
-          if (convertedSelections.includes(currentSelection)) {
-            console.warn(`Already converted: ${currentSelection}`)
-            chrome.contextMenus.remove(currentSelection)
-          }
-
-          const targetDate = parseDateString(currentSelection)
-
-          chrome.contextMenus.create({
-            "parentId": "TimezoneTravelerRoot",
-            "id": currentSelection,
-            "title": currentSelection,
-            "contexts": ["all"],
-          })
-
-          targetTimezones.forEach((targetTimezone) => {
-            const timezoneDateString = formatWithTimezone(targetDate, targetTimezone)
-            chrome.contextMenus.create({
-              "parentId": currentSelection,
-              "id": `result-${currentSelection}-${targetTimezone}`,
-              "title": `${timezoneDateString} (${targetTimezone})`,
-              "contexts": ["all"],
-            })
-          })
-          if (!convertedSelections.includes(currentSelection)) {
-            convertedSelections.push(currentSelection)
-            await chrome.storage.local.set({ convertedSelections: convertedSelections })
-          }
-        } catch (error) {
-          console.warn(error)
-          return
-        }
-        return
-      }
-
-      if (menuItemId === "TimezoneTravelerClearAll") {
-        while (convertedSelections.length) {
-          const removedMenuItemId = convertedSelections.pop()
-          if (removedMenuItemId) {
-            chrome.contextMenus.remove(removedMenuItemId)
-          }
-        }
-        await chrome.storage.local.remove(["convertedSelections"])
-        return
-      }
-    }
-  )
 })
 
+chrome.contextMenus.onClicked.addListener(
+  async (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
+    const storageResults = await chrome.storage.local.get(["convertedSelections"])
+    const convertedSelections: string[] = storageResults.convertedSelections || []
+
+    const menuItemId = info.menuItemId.toString()
+    if (menuItemId === "TimezoneTravelerConvert") {
+      try {
+        const currentSelection = info.selectionText
+        if (!currentSelection) {
+          throw new Error("Text not seelcted.")
+        }
+        if (convertedSelections.includes(currentSelection)) {
+          console.warn(`Already converted: ${currentSelection}`)
+          chrome.contextMenus.remove(currentSelection)
+        }
+
+        const targetDate = parseDateString(currentSelection)
+
+        chrome.contextMenus.create({
+          "parentId": "TimezoneTravelerRoot",
+          "id": currentSelection,
+          "title": currentSelection,
+          "contexts": ["all"],
+        })
+
+        targetTimezones.forEach((targetTimezone) => {
+          const timezoneDateString = formatWithTimezone(targetDate, targetTimezone)
+          chrome.contextMenus.create({
+            "parentId": currentSelection,
+            "id": `result-${currentSelection}-${targetTimezone}`,
+            "title": `${timezoneDateString} (${targetTimezone})`,
+            "contexts": ["all"],
+          })
+        })
+        if (!convertedSelections.includes(currentSelection)) {
+          convertedSelections.push(currentSelection)
+          await chrome.storage.local.set({ convertedSelections: convertedSelections })
+        }
+      } catch (error) {
+        console.warn(error)
+        return
+      }
+      return
+    }
+
+    if (menuItemId === "TimezoneTravelerClearAll") {
+      while (convertedSelections.length) {
+        const removedMenuItemId = convertedSelections.pop()
+        if (removedMenuItemId) {
+          chrome.contextMenus.remove(removedMenuItemId)
+        }
+      }
+      await chrome.storage.local.remove(["convertedSelections"])
+      return
+    }
+  }
+)
+
 chrome.runtime.onSuspend.addListener(() => {
-  console.log("Unloading.");
-});
+  console.log("Unloading.")
+})
